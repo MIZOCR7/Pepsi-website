a3c7cef23ba485c8d6e27f82da97834ed4c0e14f
MIZOCR7 <myfreelancinglife777@gmail.com>
2026-06-06 21:21:48 +0300

    finished the main js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+let mainImage = document.getElementById('pepsi-image');
+let background = document.getElementById('home');
+let blackCan = document.getElementById('black-can');
+let whiteCan = document.getElementById('white-can');
 
+function changeToBlue() {
+    mainImage.style.transform = "scale(1.2)";
+    blackCan.style.transform = "scale(1)";
+    whiteCan.style.transform = "scale(1)";
+    
+    background.style.background = '#0162be';
+    background.style.color = '#fff';
+}
+
+function changeToBlack() {
+    mainImage.style.transform = "scale(1)";
+    blackCan.style.transform = "scale(1.2)";
+    whiteCan.style.transform = "scale(1)";
+    
+    background.style.background = '#1e1e1e';
+    background.style.color = '#fff';
+}
+
+function changeToWhite() {
+    mainImage.style.transform = "scale(1)";
+    blackCan.style.transform = "scale(1)";
+    whiteCan.style.transform = "scale(1.2)";
+    
+    background.style.background = '#e0e0e0';
+    background.style.color = '#000';
+}
+
+mainImage.onclick = changeToBlue;
+blackCan.onclick = changeToBlack;
+whiteCan.onclick = changeToWhite;
+
+window.onload = changeToBlue;
